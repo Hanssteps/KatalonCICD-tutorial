66fe23eb5f316da5c5114fd20e2da1dbd8c66d1d
hanssteps <hanssteps+github@gmail.com>
2024-04-18 17:00:06 +0200

    test

--- Test Suites/Testops integration.ts
+++ Test Suites/Testops integration.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>b98b845c-8f5e-425e-8365-0d83fe842fb8</testSuiteGuid>
+   <testCaseLink>
+      <guid>93ff6bae-cb57-48b6-a712-9602f4c4ea45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Testops integration/Testops testing</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
